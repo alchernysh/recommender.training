c04df07153939176ff29ce7cf0fd1af8b35f54af
alchernysh <aleksei.0.chernysh>
2021-04-08 00:04:05 +0300

    fix: chech metrics existence in tensorboard callback

--- src/train/callbacks/tensorboard.py
+++ src/train/callbacks/tensorboard.py
@@ -10,19 +10,26 @@ class TensorboardCallback(pl.callbacks.base.Callback):
 
     def on_batch_end(self, trainer, pl_module):
         global_step = trainer.global_step
+
         lr = trainer.lr_schedulers[0]['scheduler']._last_lr[0]
         self._writer.add_scalar('lr', lr, global_step=global_step)
+
         metrics = trainer.callback_metrics
-        loss_train = metrics['train_loss']
-        metric_train = metrics['train_metric']
-        self._writer.add_scalars('loss', {'train': loss_train}, global_step=global_step)
-        self._writer.add_scalars('metric', {'train': metric_train}, global_step=global_step)
+
+        loss_train = metrics.get('train_loss', None)
+        if loss_train is not None:
+            self._writer.add_scalars('loss', {'train': loss_train}, global_step=global_step)
+
+        metric_train = metrics.get('train_metric', None)
+        if metric_train is not None:
+            self._writer.add_scalars('metric', {'train': metric_train}, global_step=global_step)
 
     def on_validation_end(self, trainer, pl_module):
         global_step = trainer.global_step
         metrics = trainer.callback_metrics
-        if metrics != {}:
-            loss_val = metrics['val_loss']
-            metric_val = metrics['val_metric']
+        loss_val = metrics.get('val_loss', None)
+        if loss_val is not None:
             self._writer.add_scalars('loss', {'val': loss_val}, global_step=global_step)
+        metric_val = metrics.get('val_metric', None)
+        if metric_val is not None:
             self._writer.add_scalars('metric', {'val': metric_val}, global_step=global_step)
