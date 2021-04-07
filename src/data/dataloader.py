6792f39c24d234213d5a91d77276b7c24f15dee9
alchernysh <aleksei.0.chernysh>
2021-04-08 00:02:57 +0300

    feat: pass workers param in dataloader

--- src/data/dataloader.py
+++ src/data/dataloader.py
@@ -8,4 +8,4 @@ from pyxis.torch import TorchDataset
 def get_dataloader(dataset_path, dataset_name, batch_size):
     dataset_path_full = Path(dataset_path).joinpath(dataset_name)
     dataset = TorchDataset(str(dataset_path_full))
-    return DataLoader(dataset, batch_size=batch_size, shuffle=False, drop_last=True)
+    return DataLoader(dataset, batch_size=batch_size, shuffle=False, drop_last=True, num_workers=4)
